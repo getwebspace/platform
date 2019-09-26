ccdb1d3206f83db79528ab5a75b7bad4cb22b012
Aleksey Ilyin <alksily@icloud.com>
2019-09-26 22:04:47 +0300

    просмотренные товары и список файлов с выборкой #15

--- src/Application/TwigExtension.php
+++ src/Application/TwigExtension.php
@@ -4,6 +4,7 @@ namespace App\Application;
 
 use Doctrine\ORM\EntityManager;
 use Psr\Container\ContainerInterface;
+use Ramsey\Uuid\Uuid;
 use Slim\Http\Uri;
 
 class TwigExtension extends \Twig\Extension\AbstractExtension
@@ -43,34 +44,36 @@ class TwigExtension extends \Twig\Extension\AbstractExtension
 
     public function getFunctions()
     {
-        return array_merge(
-            [
-                // slim functions
-                new \Twig\TwigFunction('path_for', [$this, 'pathFor']),
-                new \Twig\TwigFunction('full_url_for', [$this, 'fullUrlFor']),
-                new \Twig\TwigFunction('base_url', [$this, 'baseUrl']),
-                new \Twig\TwigFunction('is_current_path', [$this, 'isCurrentPath']),
-                new \Twig\TwigFunction('current_path', [$this, 'currentPath']),
+        return [
+            // slim functions
+            new \Twig\TwigFunction('path_for', [$this, 'pathFor']),
+            new \Twig\TwigFunction('full_url_for', [$this, 'fullUrlFor']),
+            new \Twig\TwigFunction('base_url', [$this, 'baseUrl']),
+            new \Twig\TwigFunction('is_current_path', [$this, 'isCurrentPath']),
+            new \Twig\TwigFunction('current_path', [$this, 'currentPath']),
 
-                // 0x12f functions
-                new \Twig\TwigFunction('form', [$this, 'form'], ['is_safe' => ['html']]),
-                new \Twig\TwigFunction('reference', [$this, 'reference']),
-                new \Twig\TwigFunction('pre', [$this, 'pre']),
-                new \Twig\TwigFunction('collect', [$this, 'collect']),
-                new \Twig\TwigFunction('non_page_path', [$this, 'non_page_path']),
-                new \Twig\TwigFunction('current_page_number', [$this, 'current_page_number']),
-                new \Twig\TwigFunction('is_current_page_number', [$this, 'is_current_page_number']),
+            // 0x12f functions
+            new \Twig\TwigFunction('form', [$this, 'form'], ['is_safe' => ['html']]),
+            new \Twig\TwigFunction('reference', [$this, 'reference']),
+            new \Twig\TwigFunction('pre', [$this, 'pre']),
+            new \Twig\TwigFunction('collect', [$this, 'collect']),
+            new \Twig\TwigFunction('non_page_path', [$this, 'non_page_path']),
+            new \Twig\TwigFunction('current_page_number', [$this, 'current_page_number']),
+            new \Twig\TwigFunction('is_current_page_number', [$this, 'is_current_page_number']),
 
-                // publication functions
-                new \Twig\TwigFunction('publication', [$this, 'publication']),
-                new \Twig\TwigFunction('publication_category', [$this, 'publication_category']),
+            // publication functions
+            new \Twig\TwigFunction('files', [$this, 'files']),
 
-                // catalog functions
-                new \Twig\TwigFunction('catalog_category', [$this, 'catalog_category']),
-                new \Twig\TwigFunction('catalog_product', [$this, 'catalog_product']),
-                new \Twig\TwigFunction('catalog_order', [$this, 'catalog_order']),
-            ]
-        );
+            // publication functions
+            new \Twig\TwigFunction('publication', [$this, 'publication']),
+            new \Twig\TwigFunction('publication_category', [$this, 'publication_category']),
+
+            // catalog functions
+            new \Twig\TwigFunction('catalog_category', [$this, 'catalog_category']),
+            new \Twig\TwigFunction('catalog_product', [$this, 'catalog_product']),
+            new \Twig\TwigFunction('catalog_product_view', [$this, 'catalog_product_view']),
+            new \Twig\TwigFunction('catalog_order', [$this, 'catalog_order']),
+        ];
     }
 
     /*
@@ -236,6 +239,51 @@ class TwigExtension extends \Twig\Extension\AbstractExtension
     }
 
     /*
+     * files functions
+     */
+
+    // получает файлы по параметрам
+    public function files($data = [])
+    {
+        $default = [
+            'uuid' => '',
+            'item' => '',
+            'item_uuid' => '',
+        ];
+        $data = array_merge($default, $data);
+        $criteria = [];
+
+        if ($data['uuid']) {
+            if (!is_array($data['uuid'])) $data['uuid'] = [$data['uuid']];
+
+            foreach ($data['uuid'] as $value) {
+                if (\Ramsey\Uuid\Uuid::isValid($value) === true) {
+                    $criteria['uuid'][] = $value;
+                }
+            }
+        }
+
+        if ($data['item']) {
+            $criteria['item'] = $data['item'];
+        }
+
+        if ($data['item_uuid']) {
+            if (!is_array($data['item_uuid'])) $data['item_uuid'] = [$data['item_uuid']];
+
+            foreach ($data['item_uuid'] as $value) {
+                if (\Ramsey\Uuid\Uuid::isValid($value) === true) {
+                    $criteria['item_uuid'][] = $value;
+                }
+            }
+        }
+
+        /** @var \Doctrine\Common\Persistence\ObjectRepository|\Doctrine\ORM\EntityRepository $repository */
+        $repository = $this->entityManager->getRepository(\App\Domain\Entities\File::class);
+
+        return collect($repository->findBy($criteria));
+    }
+
+    /*
      * publication functions
      */
 
@@ -290,14 +338,18 @@ class TwigExtension extends \Twig\Extension\AbstractExtension
         $criteria = [];
 
         if ($category) {
-            switch (true) {
-                case \Ramsey\Uuid\Uuid::isValid($category) === true:
-                    $criteria['uuid'] = $category;
-                    break;
+            if (!is_array($category)) $category = [$category];
 
-                default:
-                    $criteria['address'] = $category;
-                    break;
+            foreach ($category as $value) {
+                switch (true) {
+                    case \Ramsey\Uuid\Uuid::isValid($value) === true:
+                        $criteria['uuid'][] = $value;
+                        break;
+
+                    default:
+                        $criteria['address'][] = $value;
+                        break;
+                }
             }
         }
 
@@ -305,6 +357,27 @@ class TwigExtension extends \Twig\Extension\AbstractExtension
         $repository = $this->entityManager->getRepository(\App\Domain\Entities\Catalog\Product::class);
 
         return $limit > 1 ? collect($repository->findBy($criteria, $order, $limit, $offset)) : $repository->findOneBy($criteria, $order);
+    }
+
+    // сохраняет переданный в аргумент uuid товара, если null возвращает список товаров
+    public function catalog_product_view(\Ramsey\Uuid\DegradedUuid $uuid = null, $limit = 10)
+    {
+        $list = $_SESSION['catalog_product_view'] ?? [];
+
+        switch (true) {
+            case is_null($uuid) === true:
+                return $list;
+
+            case Uuid::isValid($uuid) === true:
+                // shift first element
+                if (count($list) >= $limit) {
+                    array_shift($list);
+                }
+
+                $list[] = $uuid->toString();
+
+                $_SESSION['catalog_product_view'] = array_unique($list);
+        }
     }
 
     // получение заказа
