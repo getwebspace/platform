d28d37d6ca4fb129ef7583ee7c13136dd26eb171
Aleksey Ilyin <alksily@icloud.com>
2019-10-01 13:13:14 +0300

    add sitemap #4

---
+++ src/Domain/Tasks/Generate/SiteMapTask.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Domain\Tasks\Generate;
+
+use App\Domain\Tasks\Task;
+use Bukashk0zzz\YmlGenerator\Generator;
+use Bukashk0zzz\YmlGenerator\Model\Category;
+use Bukashk0zzz\YmlGenerator\Model\Currency;
+use Bukashk0zzz\YmlGenerator\Model\Delivery;
+use Bukashk0zzz\YmlGenerator\Model\Offer\OfferSimple;
+use Bukashk0zzz\YmlGenerator\Model\ShopInfo;
+use Bukashk0zzz\YmlGenerator\Settings;
+use samdark\sitemap\Sitemap;
+
+class SiteMapTask extends Task
+{
+    public function execute(array $params = []): \App\Domain\Entities\Task
+    {
+        $default = [
+            // nothing
+        ];
+        $params = array_merge($default, $params);
+
+        return parent::execute($params);
+    }
+
+    protected function action()
+    {
+        /**
+         * @var \Doctrine\Common\Persistence\ObjectRepository|\Doctrine\ORM\EntityRepository $pageRepository
+         * @var \Doctrine\Common\Persistence\ObjectRepository|\Doctrine\ORM\EntityRepository $categoryRepository
+         * @var \Doctrine\Common\Persistence\ObjectRepository|\Doctrine\ORM\EntityRepository $productRepository
+         */
+        $pageRepository = $this->entityManager->getRepository(\App\Domain\Entities\Page::class);
+        $categoryRepository = $this->entityManager->getRepository(\App\Domain\Entities\Catalog\Category::class);
+        $productRepository = $this->entityManager->getRepository(\App\Domain\Entities\Catalog\Product::class);
+        $data = [
+            'page' => collect($pageRepository->findAll()),
+            'category' => collect($categoryRepository->findAll()),
+            'product' => collect($productRepository->findAll()),
+        ];
+
+        $url = $this->getParameter('common_homepage', 'http://shop.0x12f.com');
+
+        // create sitemap
+        $sitemap = new Sitemap(PUBLIC_DIR . '/sitemap.xml');
+
+        // main page
+        $sitemap->addItem($url, time(), Sitemap::WEEKLY, 0.5);
+
+        // other pages
+        foreach ($data['page'] as $model) {
+            /** @var \App\Domain\Entities\Page $model */
+            $sitemap->addItem($url . $model->address, time(), Sitemap::WEEKLY, 0.3);
+        }
+
+        // catalog category
+        foreach ($data['category'] as $model) {
+            /** @var \App\Domain\Entities\Catalog\Category $model */
+            $sitemap->addItem($url . $model->address, time(), Sitemap::WEEKLY, 0.5);
+        }
+
+        // catalog products
+        foreach ($data['product'] as $model) {
+            /** @var \App\Domain\Entities\Catalog\Product $model */
+            $category = $data['category']->firstWhere('uuid', $model->category);
+
+            $urlCategory = $url . 'catalog/';
+            if ($category) {
+                $urlCategory .= $category->address;
+            }
+            $urlCategory .= '/' . $model->address;
+
+            $sitemap->addItem($urlCategory, time(), Sitemap::WEEKLY, 0.7);
+        }
+
+        $sitemap->write();
+
+        $this->status_done();
+    }
+}
