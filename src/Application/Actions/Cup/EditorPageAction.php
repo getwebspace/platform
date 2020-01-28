3d95bea6424baf7b85000e9eb6770953ea237764
Aleksey Ilyin <alksily@icloud.com>
2020-01-28 15:18:43 +0200

    редактор шаблонов, финал

--- src/Application/Actions/Cup/EditorPageAction.php
+++ src/Application/Actions/Cup/EditorPageAction.php
@@ -12,18 +12,37 @@ class EditorPageAction extends Action
         $list = collect();
         $file = null;
         $content = null;
+        $theme_dir = THEME_DIR . '/' . $this->getParameter('common_theme');
 
-        if (($path = realpath(THEME_DIR . '/' . $this->getParameter('common_theme'))) !== false) {
+        if (($path = realpath($theme_dir)) !== false) {
             $list = $this->getCatalog($path)->sortBy('type');
         }
 
-        if (isset($this->args['file']) && ($path = realpath(THEME_DIR . '/' . $this->getParameter('common_theme') . '/' . $this->resolveArg('file'))) !== false) {
+        if (isset($this->args['file']) && ($path = realpath($theme_dir . '/' . $this->resolveArg('file'))) !== false) {
             $file = \App\Domain\Entities\File::info($path);
+            $file['path'] = str_replace($theme_dir . '/', '', $file['dir'] . '/' . $file['name'] . '.' . $file['ext']);
             $content = file_get_contents($path);
         }
 
         if ($this->request->isPost()) {
-            $data = $this->request->getParam('content');
+            $path = str_replace('..', '', $this->request->getParam('path'));
+            $absolute_path = $theme_dir . '/' . $path;
+            $content = $this->request->getParam('content');
+
+            // удаление файла
+            if ($this->request->getParam('save', 'exit') === 'delete' && file_exists($absolute_path)) {
+                unlink($absolute_path);
+
+                return $this->response->withAddedHeader('Location', '/cup/editor')->withStatus(301);
+            }
+
+            if (!file_exists($absolute_path)) {
+                mkdir(dirname($absolute_path), 0777, true);
+            }
+
+            file_put_contents($absolute_path, $content);
+
+            return $this->response->withAddedHeader('Location', '/cup/editor/' . $path)->withStatus(301);
         }
 
         return $this->respondRender('cup/editor/index.twig', ['list' => $list, 'file' => $file, 'content' => $content]);
