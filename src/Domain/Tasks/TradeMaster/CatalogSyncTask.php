2cfa6502c03b2a28d19266ceb5437a031faaecdd
Aleksey Ilyin <alksily@icloud.com>
2020-03-10 12:37:19 +0200

    revert field value

--- src/Domain/Tasks/TradeMaster/CatalogSyncTask.php
+++ src/Domain/Tasks/TradeMaster/CatalogSyncTask.php
@@ -185,7 +185,7 @@ class CatalogSyncTask extends Task
                         'order' => $item['poryadok'],
                         'description' => urldecode($item['opisanie']),
                         'extra' => urldecode($item['opisanieDop']),
-                        'address' => '',
+                        'address' => $item['link'],
                         'field1' => $item['ind1'],
                         'field2' => $item['ind2'],
                         'field3' => $item['ind3'],
