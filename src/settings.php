43a2fef385334664f5430f53ee16ef8cd6297e73
Aleksey Ilyin <alksily@icloud.com>
2020-04-18 16:36:00 +0300

    change

--- src/settings.php
+++ src/settings.php
@@ -41,7 +41,7 @@ $settings = [
 
     'settings' => [
         'sentry' => ($_ENV['SENTRY'] ?? null),
-        'displayErrorDetails' => ((bool) $_ENV['DEBUG'] ?? false), // set to false in production
+        'displayErrorDetails' => (bool) ($_ENV['DEBUG'] ?? false), // set to false in production
         'addContentLengthHeader' => false, // allow the web server to send the content-length header
         'determineRouteBeforeAppMiddleware' => true,
 
