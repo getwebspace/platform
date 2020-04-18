679ee9efd87d56739816a79d45eacacebf648d71
Aleksey Ilyin <alksily@icloud.com>
2020-04-18 14:36:57 +0300

    fix namespace

--- tests/Domain/Service/User/UserServiceTest.php
+++ tests/Domain/Service/User/UserServiceTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Tests\Domain\Service\User;
+namespace tests\Domain\Service\User;
 
 use App\Domain\Entities\User;
 use App\Domain\Entities\User\Session as UserSession;
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Tests;
+namespace tests;
 
 abstract class TestCase extends \PHPUnit\Framework\TestCase
 {
