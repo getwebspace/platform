6ceed0e5225808ac9188b46bd85de7f63ca6161c
Aleksey Ilyin <alksily@icloud.com>
2020-06-09 16:04:22 +0300

    fix upload and convert files

--- src/Domain/Tasks/ConvertImageTask.php
+++ src/Domain/Tasks/ConvertImageTask.php
@@ -2,8 +2,9 @@
 
 namespace App\Domain\Tasks;
 
-use Alksily\Support\Str;
 use App\Domain\AbstractTask;
+use App\Domain\Service\File\Exception\FileNotFoundException;
+use App\Domain\Service\File\FileService;
 
 class ConvertImageTask extends AbstractTask
 {
@@ -21,65 +22,68 @@ class ConvertImageTask extends AbstractTask
 
     protected function action(array $args = []): void
     {
-        /** @var \Doctrine\Common\Persistence\ObjectRepository|\Doctrine\ORM\EntityRepository $fileRepository */
-        $fileRepository = $this->entityManager->getRepository(\App\Domain\Entities\File::class);
+        $fileService = FileService::getWithContainer($this->container);
 
-        $files = $fileRepository->findBy(['uuid' => $args['uuid']]);
+        foreach ((array) $args['uuid'] as $index => $uuid) {
+            try {
+                $file = $fileService->read(['uuid' => $uuid]);
+                $this->logger->info('Task: info', $file->toArray());
 
-        /** @var \App\Domain\Entities\File $file */
-        foreach ($files as $index => $file) {
-            if (Str::start('image/', $file->getType())) {
-                $folder = $file->getDir('');
-                $original = $file->getInternalPath();
+                if (str_starts_with('image/', $file->getType())) {
+                    $folder = $file->getDir('');
+                    $original = $file->getInternalPath();
 
-                $command = $this->getParameter('image_convert_bin', '/usr/bin/convert');
-                $params = [
-                    '-sampling-factor 4:2:0',
-                    '-strip',
-                    '-quality 75%',
-                    '-depth 8',
-                    '-define jpeg:extent=300k',
-                    '-interlace JPEG',
-                    '-colorspace RGB',
-                    '-background white',
-                    '-alpha remove',
-                    '-alpha off',
-                    '-set comment "Converted in WebSpace Engine CMS"',
-                ];
+                    $command = $this->getParameter('image_convert_bin', '/usr/bin/convert');
+                    $params = [
+                        '-sampling-factor 4:2:0',
+                        '-strip',
+                        '-quality 75%',
+                        '-depth 8',
+                        '-define jpeg:extent=300k',
+                        '-interlace JPEG',
+                        '-colorspace RGB',
+                        '-background white',
+                        '-alpha remove',
+                        '-alpha off',
+                        '-set comment "Converted in WebSpace Engine CMS"',
+                    ];
 
-                foreach (
-                    [
-                        'middle' => $this->getParameter('image_convert_size_middle', 450),
-                        'small' => $this->getParameter('image_convert_size_small', 200),
-                    ] as $size => $pixels
-                ) {
-                    if ($pixels > 0) {
-                        $path = $folder . '/' . $size;
+                    foreach (
+                        [
+                            'middle' => $this->getParameter('image_convert_size_middle', 450),
+                            'small' => $this->getParameter('image_convert_size_small', 200),
+                        ] as $size => $pixels
+                    ) {
+                        if ($pixels > 0) {
+                            $path = $folder . '/' . $size;
 
-                        if (!file_exists($path)) {
-                            mkdir($path, 0777, true);
+                            if (!file_exists($path)) {
+                                mkdir($path, 0777, true);
+                            }
+
+                            $buf = array_merge($params, ['-resize x' . $pixels . '\>']);
+                            @exec($command . " '" . $original . "' " . implode(' ', $buf) . " '" . $path . '/' . $file->getName() . ".jpg'");
+                            $this->logger->info('Task: convert image', ['size' => $size, 'salt' => $file->getSalt(), 'params' => $buf]);
                         }
-
-                        $buf = array_merge($params, ['-resize x' . $pixels . '\>']);
-                        @exec($command . " '" . $original . "' " . implode(' ', $buf) . " '" . $path . '/' . $file->getName() . ".jpg'");
-                        $this->logger->info('Task: convert image', ['size' => $size, 'salt' => $file->getSalt(), 'params' => $buf]);
                     }
+
+                    @exec($command . " '" . $original . "' " . implode(' ', $params) . " '" . $folder . '/' . $file->getName() . ".jpg'");
+                    $this->logger->info('Task: convert image', ['size' => 'original', 'salt' => $file->getSalt(), 'params' => $params]);
+
+                    // установка расширения файла и типа
+                    if ($file->getExt() !== 'jpg') {
+                        $file->setExt('jpg');
+                        $file->setType('image/jpeg; charset=binary');
+                    }
+
+                    // обновление размера файла
+                    $file->setSize(filesize($folder . '/' . $file->getName() . '.jpg'));
                 }
-
-                @exec($command . " '" . $original . "' " . implode(' ', $params) . " '" . $folder . '/' . $file->getName() . ".jpg'");
-                $this->logger->info('Task: convert image', ['size' => 'original', 'salt' => $file->getSalt(), 'params' => $params]);
-
-                // установка расширения файла и типа
-                if ($file->getExt() !== 'jpg') {
-                    $file->setExt('jpg');
-                    $file->setType('image/jpeg; charset=binary');
-                }
-
-                // обновление размера файла
-                $file->setSize(filesize($folder . '/' . $file->getName() . '.jpg'));
+            } catch (FileNotFoundException $e) {
+                $this->logger->alert('Task: file not found');
             }
 
-            $this->setProgress($index, count($files));
+            $this->setProgress($index, count($args['uuid']));
         }
 
         $this->setStatusDone();
