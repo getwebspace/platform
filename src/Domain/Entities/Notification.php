e6691084419acdc39f0edf81bac0a6ad880ab90f
Aleksey Ilyin <alksily@icloud.com>
2020-07-17 15:24:39 +0300

    start work with notification

--- src/Domain/Entities/Notification.php
+++ src/Domain/Entities/Notification.php
@@ -3,6 +3,7 @@
 namespace App\Domain\Entities;
 
 use App\Domain\AbstractEntity;
+use App\Domain\Entities\Form\Data;
 use DateTime;
 use Doctrine\ORM\Mapping as ORM;
 use Ramsey\Uuid\Uuid;
@@ -20,34 +21,147 @@ class Notification extends AbstractEntity
      * @ORM\GeneratedValue(strategy="CUSTOM")
      * @ORM\CustomIdGenerator(class="Ramsey\Uuid\Doctrine\UuidGenerator")
      */
-    public $uuid;
+    protected Uuid $uuid;
 
     /**
-     * @var Uuid
+     * @return Uuid
+     */
+    public function getUuid(): Uuid
+    {
+        return $this->uuid;
+    }
+
+    /**
+     * @var string|Uuid
      * @ORM\Column(type="uuid", options={"default": \Ramsey\Uuid\Uuid::NIL})
      */
-    public $user_uuid = \Ramsey\Uuid\Uuid::NIL;
+    protected $user_uuid = \Ramsey\Uuid\Uuid::NIL;
 
     /**
-     * @var string
+     * @param string|Uuid $uuid
+     *
+     * @throws \Exception
+     *
+     * @return $this
+     */
+    public function setUserUuid($uuid)
+    {
+        $this->user_uuid = $this->getUuidByValue($uuid);
+
+        return $this;
+    }
+
+    /**
+     * @return Uuid
+     */
+    public function getUserUuid(): Uuid
+    {
+        return $this->user_uuid;
+    }
+
+    /**
      * @ORM\Column(type="string", options={"default": ""})
      */
-    public $title = '';
+    protected string $title = '';
 
     /**
-     * @var string
+     * @param string $title
+     *
+     * @return $this
+     */
+    public function setTitle(string $title)
+    {
+        if ($this->checkStrLenMax($title, 50)) {
+            $this->title = $title;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
      * @ORM\Column(type="text", options={"default": ""})
      */
-    public $message = '';
+    protected string $message = '';
 
     /**
+     * @param string $message
+     *
+     * @return $this
+     */
+    public function setMessage(string $message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    /**
+     * @var array
      * @ORM\Column(type="array")
      */
-    public $params = [];
+    protected array $params = [];
+
+    /**
+     * @param array $params
+     *
+     * @return $this
+     */
+    public function setParams(array $params)
+    {
+        $this->params = $params;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getParams(): array
+    {
+        return $this->params;
+    }
 
     /**
      * @var DateTime
      * @ORM\Column(type="datetime", options={"default": "CURRENT_TIMESTAMP"})
      */
-    public $date;
+    protected DateTime $date;
+
+    /**
+     * @param $date
+     *
+     * @throws \Exception
+     *
+     * @return $this
+     */
+    public function setDate($date)
+    {
+        $this->date = $this->getDateTimeByValue($date);
+
+        return $this;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
 }
