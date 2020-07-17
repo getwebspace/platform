9be175d2bbc31680026e3dfbfe610bae0358aa4b
Aleksey Ilyin <alksily@icloud.com>
2020-07-17 15:56:34 +0300

    add repos

---
+++ src/Domain/Repository/NotificationRepository.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+namespace App\Domain\Repository;
+
+use App\Domain\AbstractRepository;
+use App\Domain\Entities\Notification;
+use App\Domain\Entities\Task;
+use App\Domain\Entities\User;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\NonUniqueResultException;
+
+/**
+ * @method null|Notification findOneByUuid($uuid)
+ * @method null|Notification find($id, $lockMode = null, $lockVersion = null)
+ * @method null|Notification findOneBy(array $criteria, array $orderBy = null)
+ * @method Notification[]    findAll()
+ * @method Notification[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class NotificationRepository extends AbstractRepository
+{
+}
