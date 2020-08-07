26c8cf8679ea12e34789d011a3d626a61d6dbe50
Aleksey Ilyin <alksily@icloud.com>
2020-08-07 14:29:47 +0300

    remove controller

--- src/routes.php
+++ src/routes.php
@@ -47,9 +47,6 @@ $app
     ->group('/cup', function (App $app): void {
         $app->map(['get', 'post'], '/login', \App\Application\Actions\Cup\LoginPageAction::class);
 
-        // catalog scan
-        $app->get('/catalog/product/scan/{channel}', \App\Application\Actions\Cup\Catalog\CatalogScanAction::class);
-
         $app
             ->group('', function (App $app): void {
                 // main page
