fefda2a5ec3a4589c9dcb0a1b6d54c1c9cdb2061
Aleksey Ilyin <alksily@icloud.com>
2020-12-03 13:11:12 +0200

    change redirect

--- src/middleware.php
+++ src/middleware.php
@@ -21,7 +21,7 @@ $app->add(function (Request $request, Response $response, $next) {
     $path = $request->getUri()->getPath();
 
     if ($path !== '/' && str_end_with($path, '/')) {
-        return $response->withAddedHeader('Location', rtrim($path, '/'))->withStatus(308);
+        return $response->withRedirect(rtrim($path, '/'));
     }
 
     return $next($request, $response);
