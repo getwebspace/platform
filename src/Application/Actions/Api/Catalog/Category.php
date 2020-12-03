3eaf2dc5255def0c55c960f717112c9c59e36e16
Aleksey Ilyin <alksily@icloud.com>
2020-12-03 15:56:29 +0200

    fix catalog api

--- src/Application/Actions/Api/Catalog/Category.php
+++ src/Application/Actions/Api/Catalog/Category.php
@@ -9,19 +9,17 @@ class Category extends CatalogAction
     protected function action(): \Slim\Http\Response
     {
         $catalogCategoryService = CatalogCatalogService::getWithContainer($this->container);
-        $categories = $catalogCategoryService
-            ->read([
-                'uuid' => $this->request->getParam('uuid'),
-                'parent' => $this->request->getParam('parent'),
-                'address' => $this->request->getParam('address'),
-                'status' => $this->request->getParam('status', \App\Domain\Types\Catalog\CategoryStatusType::STATUS_WORK),
-                'external_id' => $this->request->getParam('external_id'),
+        $categories = from_service_to_array($catalogCategoryService->read([
+            'uuid' => $this->request->getParam('uuid'),
+            'parent' => $this->request->getParam('parent'),
+            'address' => $this->request->getParam('address'),
+            'status' => $this->request->getParam('status', \App\Domain\Types\Catalog\CategoryStatusType::STATUS_WORK),
+            'external_id' => $this->request->getParam('external_id'),
 
-                'order' => $this->request->getParam('order', []),
-                'limit' => $this->request->getParam('limit', 1000),
-                'offset' => $this->request->getParam('offset', 0),
-            ])
-            ->toArray();
+            'order' => $this->request->getParam('order', []),
+            'limit' => $this->request->getParam('limit', 1000),
+            'offset' => $this->request->getParam('offset', 0),
+        ]));
 
         /** @var \App\Domain\Entities\Catalog\Category $category */
         foreach ($categories as &$category) {
--- src/Application/Actions/Api/Catalog/Product.php
+++ src/Application/Actions/Api/Catalog/Product.php
@@ -9,21 +9,19 @@ class Product extends CatalogAction
     protected function action(): \Slim\Http\Response
     {
         $catalogProductService = CatalogProductService::getWithContainer($this->container);
-        $products = $catalogProductService
-            ->read([
-                'uuid' => $this->request->getParam('uuid'),
-                'category' => $this->request->getParam('category'),
-                'address' => $this->request->getParam('address'),
-                'status' => $this->request->getParam('status', \App\Domain\Types\Catalog\ProductStatusType::STATUS_WORK),
-                'vendorcode' => $this->request->getParam('vendorcode'),
-                'barcode' => $this->request->getParam('barcode'),
-                'external_id' => $this->request->getParam('external_id'),
+        $products = from_service_to_array($catalogProductService->read([
+            'uuid' => $this->request->getParam('uuid'),
+            'category' => $this->request->getParam('category'),
+            'address' => $this->request->getParam('address'),
+            'status' => $this->request->getParam('status', \App\Domain\Types\Catalog\ProductStatusType::STATUS_WORK),
+            'vendorcode' => $this->request->getParam('vendorcode'),
+            'barcode' => $this->request->getParam('barcode'),
+            'external_id' => $this->request->getParam('external_id'),
 
-                'order' => $this->request->getParam('order', []),
-                'limit' => $this->request->getParam('limit', 1000),
-                'offset' => $this->request->getParam('offset', 0),
-            ])
-            ->toArray();
+            'order' => $this->request->getParam('order', []),
+            'limit' => $this->request->getParam('limit', 1000),
+            'offset' => $this->request->getParam('offset', 0),
+        ]));
 
         /** @var \App\Domain\Entities\Catalog\Product $product */
         foreach ($products as &$product) {
