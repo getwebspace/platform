0489e7b28a11175ab1d2f73331814b390bc6c6fb
Aleksey Ilyin <alksily@icloud.com>
2020-12-06 18:06:20 +0200

    fix access to dynamic file

--- public/index.php
+++ public/index.php
@@ -1,13 +1,16 @@
 <?php declare(strict_types=1);
 
 if (PHP_SAPI === 'cli-server') {
-    // To help the built-in PHP dev server, check if the request was actually for
-    // something which should probably be served as a static file
+    // To help the built-in PHP dev server, check if the request was actually:
+    // for something which should probably be served as a static file
     $url = parse_url($_SERVER['REQUEST_URI']);
     $file = __DIR__ . $url['path'];
     if (is_file($file)) {
         return false;
     }
+
+    // for something served as a dynamic file
+    $_SERVER['SCRIPT_NAME'] = '';
 }
 
 // Include global const's
