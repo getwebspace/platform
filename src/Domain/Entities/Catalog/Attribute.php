93e9bb11e84802fd96dd722270588c580dd779c8
Aleksey Ilyin <alksily@icloud.com>
2020-12-10 15:20:09 +0200

    add new entity Attribute and ProductAttribute

---
+++ src/Domain/Entities/Catalog/Attribute.php
@@ -0,0 +1,86 @@
+<?php declare(strict_types=1);
+
+namespace App\Domain\Entities\Catalog;
+
+use App\Domain\AbstractEntity;
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+use Ramsey\Uuid\Uuid;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="catalog_attribute")
+ */
+class Attribute extends AbstractEntity
+{
+    /**
+     * @var Uuid
+     * @ORM\Id
+     * @ORM\Column(type="uuid")
+     * @ORM\GeneratedValue(strategy="CUSTOM")
+     * @ORM\CustomIdGenerator(class="Ramsey\Uuid\Doctrine\UuidGenerator")
+     */
+    protected Uuid $uuid;
+
+    /**
+     * @return Uuid
+     */
+    public function getUuid(): Uuid
+    {
+        return $this->uuid;
+    }
+
+    /**
+     * @ORM\Column(type="string", length=255, options={"default": ""})
+     */
+    protected string $title = '';
+
+    /**
+     * @param string $title
+     *
+     * @return $this
+     */
+    public function setTitle(string $title)
+    {
+        if ($this->checkStrLenMax($title, 255)) {
+            $this->title = $title;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @ORM\Column(type="string", length=255, options={"default": "string"})
+     */
+    protected string $type = 'string';
+
+    /**
+     * @param string $type
+     *
+     * @return $this
+     */
+    public function setType(string $type)
+    {
+        if ($this->checkStrLenMax($type, 255) && in_array($type, ['string', 'integer', 'float'])) {
+            $this->type = $type;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+}
