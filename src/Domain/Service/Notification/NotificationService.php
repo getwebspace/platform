ff6a770441a9a006ca25cbaf4176672320e8399e
Aleksey Ilyin <alksily@icloud.com>
2020-12-23 00:06:12 +0200

    fix check user_uuid

--- src/Domain/Service/Notification/NotificationService.php
+++ src/Domain/Service/Notification/NotificationService.php
@@ -44,7 +44,7 @@ class NotificationService extends AbstractService
         ];
         $data = array_merge($default, $data);
 
-        if (!$data['user_uuid']) {
+        if (blank($data['user_uuid'])) {
             throw new MissingUserUuidValueException();
         }
         if (!$data['title']) {
