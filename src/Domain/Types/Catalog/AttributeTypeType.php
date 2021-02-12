940a1c9dec7903b2ea1f44a990680e2c1ec93d0b
Aleksey Ilyin <alksily@icloud.com>
2021-02-12 14:20:28 +0200

    add boolean type attribute

--- src/Domain/Types/Catalog/AttributeTypeType.php
+++ src/Domain/Types/Catalog/AttributeTypeType.php
@@ -11,10 +11,12 @@ class AttributeTypeType extends AbstractEnumType
     public const TYPE_STRING = 'string';
     public const TYPE_INTEGER = 'integer';
     public const TYPE_FLOAT = 'float';
+    public const TYPE_BOOLEAN = 'boolean';
 
     public const LIST = [
         self::TYPE_STRING,
         self::TYPE_INTEGER,
         self::TYPE_FLOAT,
+        self::TYPE_BOOLEAN,
     ];
 }
