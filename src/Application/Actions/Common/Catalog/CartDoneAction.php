bdd48d5a6c034badbfcadff80e4d22fc1224fb12
Aleksey Ilyin <alksily@icloud.com>
2021-03-09 14:16:28 +0200

    fix order not found exception

--- src/Application/Actions/Common/Catalog/CartDoneAction.php
+++ src/Application/Actions/Common/Catalog/CartDoneAction.php
@@ -2,6 +2,7 @@
 
 namespace App\Application\Actions\Common\Catalog;
 
+use App\Domain\Service\Catalog\Exception\OrderNotFoundException;
 use Slim\Http\Response;
 
 class CartDoneAction extends CatalogAction
@@ -15,15 +16,19 @@ class CartDoneAction extends CatalogAction
     protected function action(): \Slim\Http\Response
     {
         if ($this->resolveArg('order') && \Ramsey\Uuid\Uuid::isValid($this->resolveArg('order'))) {
-            $order = $this->catalogOrderService->read(['uuid' => $this->resolveArg('order')]);
+            try {
+                $order = $this->catalogOrderService->read(['uuid' => $this->resolveArg('order')]);
 
-            if ($order) {
-                $products = $this->catalogProductService->read(['uuid' => array_keys($order->getList())]);
+                if ($order) {
+                    $products = $this->catalogProductService->read(['uuid' => array_keys($order->getList())]);
 
-                return $this->respond($this->parameter('catalog_cart_complete_template', 'catalog.cart.complete.twig'), [
-                    'order' => $order,
-                    'products' => $products,
-                ]);
+                    return $this->respond($this->parameter('catalog_cart_complete_template', 'catalog.cart.complete.twig'), [
+                        'order' => $order,
+                        'products' => $products,
+                    ]);
+                }
+            } catch (OrderNotFoundException $e) {
+                // skip, redirect to cart
             }
         }
 
