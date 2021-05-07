bfd6064fc0fc58c847d2043ac537f05aea767e9b
Aleksey Ilyin <alksily@icloud.com>
2021-05-07 20:55:25 +0300

    fix migration comment

--- src/Migrations/Version20210502115441.php
+++ src/Migrations/Version20210502115441.php
@@ -14,7 +14,7 @@ final class Version20210502115441 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Add publication user_uuid field';
     }
 
     public function up(Schema $schema): void
