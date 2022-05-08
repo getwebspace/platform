ae3057564febb3886aebcdce08cec50b457eb3a4
Aleksey Ilyin <alksily@icloud.com>
2022-05-08 14:55:31 +0300

    fix invoce template

--- src/Application/Actions/Cup/Catalog/CatalogAction.php
+++ src/Application/Actions/Cup/Catalog/CatalogAction.php
@@ -63,54 +63,54 @@ abstract class CatalogAction extends AbstractAction
 
 // todo переместить в более удобное место
 const INVOICE_TEMPLATE = <<<'EOD'
-<div class="m-5">
-    <div class="row">
-        <div class="col-12 text-center">
-            <h3 class="font-weight-bold">{{ 'Инвойс'|locale }}</h3>
-            {#<img src="/images/logo.png" style="width: 100%; max-width: 300px" />#}
+    <div class="m-5">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h3 class="font-weight-bold">{{ 'Инвойс'|locale }}</h3>
+                {#<img src="/images/logo.png" style="width: 100%; max-width: 300px" />#}
+            </div>
+            <div class="col-6">
+                {{ parameter('common_title') }}<br />
+                {{ 'Заказ'|locale }}: <b>{{ order.external_id ?: order.serial }}</b><br />
+                {{ 'Дата'|locale }}: <b>{{ order.date|df('d.m.Y H:i') }}</b><br />
+                {{ 'Доставка'|locale }}: <b>{{ order.shipping|df('d.m.Y H:i') }}</b>
+            </div>
+            <div class="col-6 text-right">
+                {{ qr_code(base_url() ~ '/cart/done/' ~ order.uuid, 100) }}
+            </div>
         </div>
-        <div class="col-6">
-            {{ parameter('common_title') }}<br />
-            {{ 'Заказ'|locale }}: <b>{{ order.external_id ?: order.serial }}</b><br />
-            {{ 'Дата'|locale }}: <b>{{ order.date|df('d.m.Y H:i') }}</b><br />
-            {{ 'Доставка'|locale }}: <b>{{ order.shipping|df('d.m.Y H:i') }}</b>
+    
+        <div class="row mt-3">
+            <div class="col-6">
+                {{ order.user and order.user.getName() == order.delivery.client ? order.user.getName() : order.delivery.client }}<br />
+                {{ order.user and order.user.phone == order.phone ? order.user.phone : (order.phone ? order.phone : '-') }}<br />
+                {{ order.user and order.user.email == order.email ? order.user.email : (order.email ? order.email : '-') }}
+            </div>
+            <div class="col-6 text-right">
+                {{ order.user and order.user.address == order.delivery.address ? order.user.address : order.delivery.address }}<br />
+                {{ order.comment }}
+            </div>
         </div>
-        <div class="col-6 text-right">
-            {{ qr_code(base_url() ~ '/cart/done/' ~ order.uuid, 100) }}
+    
+        <div class="row py-1 mt-3 bg-grey2">
+            <div class="col-8 col-md-6 text-nowrap font-weight-bold">{{ 'Позиция'|locale }}</div>
+            <div class="d-none d-md-block col-md-2 text-right text-nowrap font-weight-bold">{{ 'Цена'|locale }}</div>
+            <div class="d-none d-md-block col-md-2 text-right text-nowrap font-weight-bold">{{ 'Количество'|locale }}</div>
+            <div class="col-4 col-md-2 text-right text-nowrap font-weight-bold">{{ 'Сумма'|locale }}</div>
+        </div>
+    
+        {% set total = 0 %}
+        {% for item in order.products %}
+            <div class="row py-1 {{ loop.last ?: 'border-bottom' }} {{ loop.index0 % 2 ? 'bg-grey1' }}">
+                <div class="col-8 col-md-6 overflow-hidden text-nowrap">{{ item.title }}</div>
+                <div class="d-none d-md-block col-md-2 text-right text-nowrap">{{ item.price|number_format(2, '.', ' ') }}</div>
+                <div class="d-none d-md-block col-md-2 text-right text-nowrap">{{ item.count }}</div>
+                <div class="col-4 col-md-2 text-right text-nowrap">{{ (item.price * item.count)|number_format(2, '.', ' ') }}</div>
+            </div>
+        {% endfor %}
+    
+        <div class="row py-1">
+            <div class="col-12 text-right text-nowrap font-weight-bold border-top">{{ 'Общая сумма'|locale }}: {{ order.getTotalPrice()|number_format(2, '.', ' ') }}</div>
         </div>
     </div>
-
-    <div class="row mt-3">
-        <div class="col-6">
-            {{ order.user and order.user.getName() == order.delivery.client ? order.user.getName() : order.delivery.client }}<br />
-            {{ order.user and order.user.phone == order.phone ? order.user.phone : (order.phone ? order.phone : '&mdash;') }}<br />
-            {{ order.user and order.user.email == order.email ? order.user.email : (order.email ? order.email : '&mdash;') }}
-        </div>
-        <div class="col-6 text-right">
-            {{ order.user and order.user.address == order.delivery.address ? order.user.address : order.delivery.address }}<br />
-            {{ order.comment }}
-        </div>
-    </div>
-
-    <div class="row py-1 mt-3 bg-grey2">
-        <div class="col-8 col-md-6 text-nowrap font-weight-bold">{{ 'Позиция'|locale }}</div>
-        <div class="d-none d-md-block col-md-2 text-right text-nowrap font-weight-bold">{{ 'Цена'|locale }}</div>
-        <div class="d-none d-md-block col-md-2 text-right text-nowrap font-weight-bold">{{ 'Количество'|locale }}</div>
-        <div class="col-4 col-md-2 text-right text-nowrap font-weight-bold">{{ 'Сумма'|locale }}</div>
-    </div>
-
-    {% set total = 0 %}
-    {% for item in order.products %}
-        <div class="row py-1 {{ loop.last ?: 'border-bottom' }} {{ loop.index0 % 2 ? 'bg-grey1' }}">
-            <div class="col-8 col-md-6 overflow-hidden text-nowrap">{{ item.title }}</div>
-            <div class="d-none d-md-block col-md-2 text-right text-nowrap">{{ item.price|number_format(2, '.', ' ') }}</div>
-            <div class="d-none d-md-block col-md-2 text-right text-nowrap">{{ item.count }}</div>
-            <div class="col-4 col-md-2 text-right text-nowrap">{{ (item.price * item.count)|number_format(2, '.', ' ') }}</div>
-        </div>
-    {% endfor %}
-
-    <div class="row py-1">
-        <div class="col-12 text-right text-nowrap font-weight-bold border-top">{{ 'Общая сумма'|locale }}: {{ order.getTotalPrice()|number_format(2, '.', ' ') }}</div>
-    </div>
-</div>
 EOD;
