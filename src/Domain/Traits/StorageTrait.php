cce968f2fbaf1c2e983e3c3fe3718fac7e91f575
Aleksey Ilyin <alksily@icloud.com>
2022-06-03 16:08:43 +0300

    add check method has key

--- src/Domain/Traits/StorageTrait.php
+++ src/Domain/Traits/StorageTrait.php
@@ -9,6 +9,11 @@ trait StorageTrait
      */
     protected static array $storage = [];
 
+    protected static function hasStorage(string $key, bool $strict = false): bool
+    {
+        return in_array($key, static::$storage, true);
+    }
+
     protected static function setStorage(string $key, mixed $value, ?string $namespace = null): mixed
     {
         self::$storage[$namespace][$key] = $value;
@@ -16,9 +21,6 @@ trait StorageTrait
         return $value;
     }
 
-    /**
-     * @param mixed       $default
-     */
     protected static function getStorage(string $key, mixed $default = null, ?string $namespace = null): mixed
     {
         return self::$storage[$namespace][$key] ?? $default;
