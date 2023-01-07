af8e06b9f859e01a79d2b714ce833454eeecef66
Aleksey Ilyin <alksily@outlook.com>
2023-01-07 14:58:05 +0200

    changes in catalog product list

--- src/Application/Actions/Cup/Catalog/Product/ProductListAction.php
+++ src/Application/Actions/Cup/Catalog/Product/ProductListAction.php
@@ -8,38 +8,19 @@ class ProductListAction extends CatalogAction
 {
     protected function action(): \Slim\Psr7\Response
     {
-        $category = null;
-
-        if (!empty($this->args['category'])) {
-            if (
-                $this->resolveArg('category') !== \Ramsey\Uuid\Uuid::NIL
-                && \Ramsey\Uuid\Uuid::isValid($this->resolveArg('category'))
-            ) {
-                /** @var \App\Domain\Entities\Catalog\Category $category */
-                $category = $this->catalogCategoryService->read([
-                    'uuid' => $this->resolveArg('category'),
-                    'status' => \App\Domain\Types\Catalog\CategoryStatusType::STATUS_WORK,
-                ]);
-            } else {
-                return $this->respondWithRedirect('/cup/catalog/product');
-            }
-        }
-
         $categories = $this->catalogCategoryService->read([
             'status' => \App\Domain\Types\Catalog\CategoryStatusType::STATUS_WORK,
         ]);
         $products = $this->catalogProductService->read([
-            'category' => $category ? $category->getNested($categories)->pluck('uuid')->all() : null,
             'status' => \App\Domain\Types\Catalog\ProductStatusType::STATUS_WORK,
             'order' => [
+                'category' => 'ASC',
                 'order' => 'ASC',
-                'title' => 'ASC',
             ],
         ]);
 
         return $this->respondWithTemplate('cup/catalog/product/index.twig', [
             'categories' => $categories,
-            'category' => $category,
             'products' => $products,
         ]);
     }
