1a6e575c2ab06e2c3c403373626fb2ece7afa3de
Aleksey Ilyin <alksily@outlook.com>
2023-01-20 17:31:35 +0200

    added optional check IP and User-Agent when user authorized

--- src/Application/Middlewares/AuthorizationMiddleware.php
+++ src/Application/Middlewares/AuthorizationMiddleware.php
@@ -18,25 +18,45 @@ class AuthorizationMiddleware extends AbstractMiddleware
         $data = [
             'uuid' => $request->getCookieParams()['uuid'] ?? null,
             'session' => $request->getCookieParams()['session'] ?? null,
+            'agent' => $request->getServerParams()['HTTP_USER_AGENT'] ?? '',
+            'ip' => $request->getServerParams()['HTTP_X_REAL_IP'] ??
+                    $request->getServerParams()['HTTP_X_FORWARDED_FOR'] ??
+                    $request->getServerParams()['REMOTE_ADDR'] ??
+                    '',
         ];
 
         if ($data['uuid'] && \Ramsey\Uuid\Uuid::isValid((string) $data['uuid']) && $data['session']) {
             try {
+                /** @var UserService $userService */
                 $userService = $this->container->get(UserService::class);
                 $user = $userService->read([
                     'uuid' => $data['uuid'],
                     'status' => \App\Domain\Types\UserStatusType::STATUS_WORK,
                 ]);
 
-                if ($user && $user->getSession()) {
-                    $hash = $user->getSession()->getHash();
+                if ($user) {
+                    $session = $user->getSession();
 
-                    if ($data['session'] === $hash) {
-                        $request = $request->withAttribute('user', $user);
+                    if ($session) {
+                        $hash = $session->getHash();
+
+                        if (
+                            $data['session'] === $hash && (
+                                $this->parameter('user_deep_check', 'no') === 'no' ||
+                                (
+                                    $session->getAgent() === $data['agent'] &&
+                                    $session->getIp() === $data['ip']
+                                )
+                            )
+                        ) {
+                            $request = $request->withAttribute('user', $user);
+
+                            return $handler->handle($request);
+                        }
                     }
-                } else {
-                    throw new \RuntimeException();
                 }
+
+                throw new \RuntimeException();
             } catch (\RuntimeException|UserNotFoundException $e) {
                 // clear cookie
                 setcookie('uuid', '-1', time(), '/');
