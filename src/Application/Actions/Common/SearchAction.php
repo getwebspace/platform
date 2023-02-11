f6d4462993bebad0fc3ef3cbadf88d49ac417012
Aleksey Ilyin <alksily@icloud.com>
2023-02-11 02:52:54 +0200

    rewrite search

--- src/Application/Actions/Common/SearchAction.php
+++ src/Application/Actions/Common/SearchAction.php
@@ -15,176 +15,55 @@ class SearchAction extends AbstractAction
 {
     protected function action(): \Slim\Psr7\Response
     {
-        $type = $this->getParam('type', $this->getParam('t', ''));
         $query = trim(str_escape($this->getParam('query', $this->getParam('q', ''))));
-        $data = Search::isPossible() ? $this->advanced($query) : $this->primitive($query);
+        $query_type = $this->getParam('type', $this->getParam('t', false));
+        $query_strong = !!$this->getParam('query_strong', $this->getParam('qs', false));
+        $limit = (int) $this->getParam('limit', $this->parameter('search_limit', 30));
+        $result = collect();
+
+        if ($query && \App\Application\Search::isPossible()) {
+            $services = [
+                'page' => $this->container->get(PageService::class),
+                'publication' => $this->container->get(PublicationService::class),
+                'catalog_product' => $this->container->get(CatalogProductService::class),
+            ];
+            $search = \App\Application\Search::search($query, $query_strong);
+
+            foreach ($services as $type => $service) {
+                if (!empty($search[$type]) && (!$query_type || (in_array($query_type, array_keys($services)) && $type === $query_type))) {
+                    $plucked = array_pluck($search[$type], 'order', 'uuid');
+                    arsort($plucked);
+
+                    /** @var AbstractService $service */
+                    $entities = $service->read([
+                        'uuid' => array_keys($plucked),
+                        'status' => 'work',
+                    ]);
+
+                    foreach ($plucked as $uuid => $order) {
+                        if (($entity = $entities->firstWhere('uuid', $uuid)) !== null) {
+                            $entity = $entity->toArray();
+                            $entity['entity'] = $type;
+
+                            if (str_starts_with($type, 'catalog_')) {
+                                $entity['address'] = $this->parameter('catalog_address', 'catalog') . '/' . $entity['address'];
+                            }
+
+                            $result[] = $entity;
+                        }
+                    }
+                }
+            }
+        }
+
+        $result = $result->slice(0, $limit);
 
         return $this->respond($this->parameter('search_template', 'search.twig'), [
-            'count' => $data['count'],
-            'result' => $type ? [$type => ($data['result'][$type] ?? [])] : $data['result'],
+            'query' => $query,
+            'type' => $query_type,
+            'strong' => $query_strong,
+            'count' => count($result),
+            'result' => $result,
         ]);
-    }
-
-    private function primitive(string $query)
-    {
-        if ($query && !$this->getParam('query_strong', $this->getParam('qs'))) {
-            $query = '%' . $query . '%';
-        }
-        $limit = (int) $this->getParam('limit', $this->parameter('search_limit', 10));
-
-        $count = 0;
-        $result = [];
-
-        if ($query) {
-            $entities = [
-                'page' => $this->container->get(PageService::class),
-                'publication' => $this->container->get(PublicationService::class),
-                'catalog_product' => $this->container->get(CatalogProductService::class),
-            ];
-
-            foreach ($entities as $type => $service) {
-                /** @var AbstractService $service */
-                $qb = $service->createQueryBuilder('e');
-
-                if (!str_starts_with($query, '%')) {
-                    $qb->andWhere('e.title = :title');
-                } else {
-                    $qb->andWhere('LOWER(e.title) LIKE LOWER(:title)');
-                }
-
-                $qb->setParameter('title', $query, \Doctrine\DBAL\ParameterType::STRING);
-
-                switch (true) {
-                    case $type === 'catalog_category':
-                        $qb->andWhere('e.status = :status')->setParameter('status', CategoryStatusType::STATUS_WORK);
-
-                        break;
-
-                    case $type === 'catalog_product':
-                        $qb->andWhere('e.status = :status')->setParameter('status', ProductStatusType::STATUS_WORK);
-
-                        break;
-                }
-
-                $qb->setMaxResults($limit);
-
-                if (($buf = $qb->getQuery()->getResult()) !== []) {
-                    foreach ($buf as $index => $entity) {
-                        $result[$type][$index] = array_intersect_key(
-                            $entity->toArray(),
-                            array_flip([
-                                'uuid', 'category',
-                                'title', 'description', 'extra', 'content',
-                                'address',
-                                'vendorcode', 'barcode',
-                                'field1', 'field2', 'field3', 'field4', 'field5',
-                                'priceFirst', 'price', 'priceWholesale',
-                                'volume', 'unit', 'external_id',
-                            ])
-                        );
-
-                        if (method_exists($entity, 'hasFiles')) {
-                            $files = [];
-
-                            /** @var \App\Domain\Entities\File $item */
-                            foreach ($entity->getFiles()->sortBy('order') as $item) {
-                                $files[] = [
-                                    'filename' => implode('.', [$item->file->name, $item->file->ext]),
-                                    'full' => $item->getPublicPath('full'),
-                                    'middle' => $item->getPublicPath('middle'),
-                                    'small' => $item->getPublicPath('small'),
-                                    'type' => $item->file->type,
-                                    'salt' => $item->file->salt,
-                                    'hash' => $item->file->hash,
-                                    'size' => $item->file->size,
-                                    'comment' => $item->comment,
-                                    'order' => $item->order,
-                                ];
-                            }
-
-                            $result[$type][$index]['files'] = $files;
-                        }
-
-                        if (str_starts_with($type, 'catalog_')) {
-                            $result[$type][$index]['address'] = $this->parameter('catalog_address', 'catalog') . '/' . $result[$type][$index]['address'];
-                        }
-                    }
-
-                    $count += count($result[$type]);
-                }
-            }
-        }
-
-        return ['count' => $count, 'result' => $result];
-    }
-
-    private function advanced(string $query)
-    {
-        if ($query && !$this->getParam('query_strong', $this->getParam('qs'))) {
-            $query = implode(' ', array_map(fn ($word) => (mb_strlen($word) > 3 ? $word . '*' : $word), explode(' ', $query)));
-        }
-        $limit = (int) $this->getParam('limit', $this->parameter('search_limit', 10));
-
-        $count = 0;
-        $result = [];
-
-        if ($query) {
-            $entities = [
-                'page' => $this->container->get(PageService::class),
-                'publication' => $this->container->get(PublicationService::class),
-                'catalog_product' => $this->container->get(CatalogProductService::class),
-            ];
-            $search_by_index = \App\Application\Search::search($query);
-
-            foreach ($entities as $type => $service) {
-                if (!empty($search_by_index[$type])) {
-                    /** @var AbstractService $service */
-                    foreach ($service->read(['uuid' => $search_by_index[$type], 'status' => 'work', 'limit' => $limit]) as $index => $entity) {
-                        $result[$type][$index] = array_intersect_key(
-                            $entity->toArray(),
-                            array_flip([
-                                'uuid', 'category',
-                                'title', 'description', 'extra', 'content',
-                                'address',
-                                'vendorcode', 'barcode',
-                                'field1', 'field2', 'field3', 'field4', 'field5',
-                                'priceFirst', 'price', 'priceWholesale',
-                                'volume', 'unit', 'external_id',
-                            ])
-                        );
-
-                        if (method_exists($entity, 'hasFiles')) {
-                            $files = [];
-
-                            /** @var \App\Domain\Entities\File $item */
-                            foreach ($entity->getFiles()->sortBy('order') as $item) {
-                                $files[] = [
-                                    'filename' => implode('.', [$item->file->name, $item->file->ext]),
-                                    'full' => $item->getPublicPath('full'),
-                                    'middle' => $item->getPublicPath('middle'),
-                                    'small' => $item->getPublicPath('small'),
-                                    'type' => $item->file->type,
-                                    'salt' => $item->file->salt,
-                                    'hash' => $item->file->hash,
-                                    'size' => $item->file->size,
-                                    'comment' => $item->comment,
-                                    'order' => $item->order,
-                                ];
-                            }
-
-                            $result[$type][$index]['files'] = $files;
-                        }
-
-                        if (str_starts_with($type, 'catalog_')) {
-                            $result[$type][$index]['address'] = $this->parameter('catalog_address', 'catalog') . '/' . $result[$type][$index]['address'];
-                        }
-                    }
-
-                    $count += count($result[$type]);
-                }
-            }
-        }
-
-        return ['count' => $count, 'result' => $result];
     }
 }
--- src/Application/Search.php
+++ src/Application/Search.php
@@ -11,68 +11,15 @@ class Search
         return file_exists(self::CACHE_FILE);
     }
 
-    public static function search(string $query): array
-    {
-        $query = static::getIndexedText($query);
-        $query_words = explode(' ', static::getIndexedText($query));
-        $index = explode(PHP_EOL, file_get_contents(self::CACHE_FILE));
-
-        $results = [];
-
-        // sort words
-        usort($query_words, fn ($word) => (str_starts_with($word, '-') || str_starts_with($word, '+')) ? 1 : -1);
-
-        foreach ($index as $line) {
-            $wordCount = 0;
-            $mustFound = 1;
-            $mustNtFound = 1;
-
-            foreach ($query_words as $word) {
-                // case '*'
-                if (mb_stristr($word, '*') && str_ends_with($word, '*')) {
-                    $search = str_replace('*', '', $word);
-                } else {
-                    $search = ' ' . $word . ' ';
-                }
-
-                // case '+'
-                if (mb_stristr($search, '+')) {
-                    $search = str_replace('+', '', $search);
-
-                    if (str_starts_with($word, '+')) {
-                        ++$mustNtFound;
-                    }
-                }
-
-                // case '-'
-                if (mb_stristr($search, '-')) {
-                    $search = str_replace('-', '', $search);
-
-                    if (str_starts_with($word, '-')) {
-                        $mustNtFound = 0;
-                    }
-                }
-
-                if (mb_stristr($line, $search)) {
-                    ++$wordCount;
-                    $wordCount = $wordCount * $mustNtFound;
-                }
-            }
-
-            if ($wordCount >= $mustFound) {
-                $buf = explode(':', $line);
-                $results[$buf[0]][] = $buf[1];
-            }
-        }
-
-        return $results;
-    }
-
     /**
+     * Prepare text
+     *
      * @param array|string $strings
-     * @param false        $indexing
+     * @param bool         $indexing
+     *
+     * @return string
      */
-    public static function getIndexedText($strings, $indexing = false): string
+    public static function getIndexedText(array|string $strings, bool $indexing = false): string
     {
         $index = [];
 
@@ -97,7 +44,7 @@ class Search
                 $text = explode(' ', $text);
 
                 foreach ($text as $i => &$word) {
-                    if (mb_strlen($word) < 3) {
+                    if (mb_strlen($word) < 2) {
                         unset($text[$i]);
                     }
                 }
@@ -109,5 +56,88 @@ class Search
         $index = array_unique($index);
 
         return trim(implode(' ', $index));
+    }
+
+    public static function search(string $query, bool $strong = false): array
+    {
+        if ($query && !$strong) {
+            $query = array_map(fn($word) => (mb_strlen($word) > 3 ? $word . '*' : $word), explode(' ', $query));
+        }
+        $query_words = explode(' ', static::getIndexedText($query));
+        $index = explode(PHP_EOL, file_get_contents(self::CACHE_FILE));
+
+        $results = [];
+
+        // sort words
+        usort($query_words, fn($word) => (str_starts_with($word, '-') || str_starts_with($word, '+')) ? 1 : -1);
+
+        foreach ($index as $line) {
+            $wordCount = 0;
+            $comboCount = 0;
+            $mustFound = 1;
+            $mustNotFound = 1;
+
+            foreach ($query_words as $word) {
+                // case '*'
+                if (mb_stristr($word, '*')) {
+                    $search = str_replace('*', '', $word);
+                } else {
+                    $search = ' ' . $word . ' ';
+                }
+
+                // case '+'
+                if (mb_stristr($search, '+')) {
+                    $search = str_replace('+', '', $search);
+
+                    if (str_starts_with($word, '+')) {
+                        ++$mustNotFound;
+                    }
+                }
+
+                // case '-'
+                if (mb_stristr($search, '-')) {
+                    $search = str_replace('-', '', $search);
+
+                    if (str_starts_with($word, '-')) {
+                        $mustNotFound = 0;
+                    }
+                }
+
+                if (mb_stristr($line, $search)) {
+                    ++$wordCount;
+                    $wordCount = $wordCount * $mustNotFound;
+                }
+            }
+
+            foreach (static::permutations($query_words) as $permutation) {
+                if (mb_stristr($line, $permutation)) {
+                    ++$comboCount;
+                }
+            }
+
+            if ($strong && !mb_stristr($line, str_replace('*', '', implode(' ', $query_words)))) {
+                $wordCount = 0;
+            }
+
+            if ($wordCount > $mustFound) {
+                $buf = explode(':', $line);
+                $results[$buf[0]][] = ['uuid' => $buf[1], 'order' => $comboCount + $wordCount];
+            }
+        }
+
+        return $results;
+    }
+
+    private static function permutations($query_words): array
+    {
+        $results = [[]];
+
+        foreach ($query_words as $k => $element) {
+            foreach ($results as $combination) {
+                $results[] = $combination + [$k => $element];
+            }
+        }
+
+        return array_map(fn($words) => str_replace('*', '', implode(' ', $words)), array_values(array_filter($results, fn($el) => count($el) >= 2)));
     }
 }
