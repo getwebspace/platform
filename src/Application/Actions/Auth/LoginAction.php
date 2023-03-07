96e4a2a37f6c9dd5ac3fd67f196c70b3f25a4cc3
Aleksey Ilyin <alksily@outlook.com>
2023-03-07 22:19:50 +0200

    fix login action

--- src/Application/Actions/Auth/LoginAction.php
+++ src/Application/Actions/Auth/LoginAction.php
@@ -12,33 +12,32 @@ class LoginAction extends AuthAction
 
     protected function action(): \Slim\Psr7\Response
     {
-        if ($this->isPost()) {
-            $identifier = $this->getParam('identifier', '');
-            $password = $this->getParam('password', '');
+        $identifier = $this->getParam('identifier', '');
+        $password = $this->getParam('password', '');
 
-            $data = [
-                'agent' => $this->getServerParam('HTTP_USER_AGENT'),
-                'ip' => $this->getRequestRemoteIP(),
-            ];
+        $data = [
+            'agent' => $this->getServerParam('HTTP_USER_AGENT'),
+            'ip' => $this->getRequestRemoteIP(),
+        ];
 
-            try {
-                $user = $this->userService->read([
-                    'identifier' => $identifier,
-                    'password' => $password,
-                    'agent' => $data['agent'],
-                    'ip' => $data['ip'],
-                    'status' => \App\Domain\Types\UserStatusType::STATUS_WORK,
+        try {
+            $user = $this->userService->read([
+                'identifier' => $identifier,
+                'password' => $password,
+                'agent' => $data['agent'],
+                'ip' => $data['ip'],
+                'status' => \App\Domain\Types\UserStatusType::STATUS_WORK,
+            ]);
+            $tokens = $this->getTokenPair($user, $data['ip'], $data['agent'], 'Login via Auth');
+            $this->container->get(\App\Application\PubSub::class)->publish('auth:user:login', $user);
+
+            return $this
+                ->respondWithJson([
+                    'access_token' => $tokens['access_token'],
+                    'refresh_token' => $tokens['refresh_token'],
                 ]);
-                $tokens = $this->getTokenPair($user, $data['ip'], $data['agent'], 'Login via Auth');
-                $this->container->get(\App\Application\PubSub::class)->publish('auth:user:login', $user);
-
-                return $this
-                    ->respondWithJson([
-                        'access_token' => $tokens['access_token'],
-                        'refresh_token' => $tokens['refresh_token'],
-                    ]);
-            } catch (UserNotFoundException|WrongPasswordException $exception) {
-            }
+        } catch (UserNotFoundException|WrongPasswordException $exception) {
+            return $this->response->withStatus(404);
         }
 
         return $this->response->withStatus(401);
