64699e54b2c77f5e4566b2bee0b1faf78bb06b87
Aleksey Ilyin <alksily@outlook.com>
2023-05-31 18:19:53 +0300

    add order list filter by status & date range

--- src/Application/Actions/Cup/Catalog/Order/OrderListAction.php
+++ src/Application/Actions/Cup/Catalog/Order/OrderListAction.php
@@ -3,15 +3,41 @@
 namespace App\Application\Actions\Cup\Catalog\Order;
 
 use App\Application\Actions\Cup\Catalog\CatalogAction;
+use Doctrine\DBAL\ParameterType;
 
 class OrderListAction extends CatalogAction
 {
     protected function action(): \Slim\Psr7\Response
     {
+        $data = [
+            'status' => $this->getParam('status', ''),
+            'from' => date(\App\Domain\References\Date::DATE, strtotime('-30 days')),
+            'to' => date(\App\Domain\References\Date::DATE),
+        ];
+        $data = array_merge($data, $this->getParam('date', []));
+
+        $qb = $this->entityManager->createQueryBuilder();
+        $query = $qb
+            ->select('o')
+            ->from(\App\Domain\Entities\Catalog\Order::class, 'o')
+            ->where('o.date >= :dateFrom')
+            ->andWhere('o.date <= :dateTo')
+            ->setParameter('dateFrom', $data['from'], ParameterType::STRING)
+            ->setParameter('dateTo', $data['to'], ParameterType::STRING)
+            ->orderBy('o.date', 'DESC');
+
+        // filter by status
+        if ($data['status']) {
+            $query
+                ->andWhere('o.status = :status')
+                ->setParameter('status', $data['status'], ParameterType::STRING);
+        }
+
         return $this->respondWithTemplate('cup/catalog/order/index.twig', [
-            'list' => $this->catalogOrderService->read([
-                'order' => ['date' => 'desc'],
-            ]),
+            'date' => ['from' => $data['from'], 'to' => $data['to']],
+            'list' => collect($query->getQuery()->getResult()),
+            'status' => $data['status'],
+            'status_list' => $this->catalogOrderStatusService->read(),
         ]);
     }
 }
