c0a2348b0fc65f456876f3710684bfa5d0f68f6e
Aleksey Ilyin <alksily@outlook.com>
2023-06-05 16:57:09 +0300

    fix check if api

--- src/Application/Middlewares/AccessCheckerMiddleware.php
+++ src/Application/Middlewares/AccessCheckerMiddleware.php
@@ -19,6 +19,9 @@ class AccessCheckerMiddleware extends AbstractMiddleware
         $routeContext = RouteContext::fromRequest($request);
         $route = $routeContext->getRoute();
 
+        // skip check if is API
+        if (str_starts_with($route->getName(), 'api')) return $handler->handle($request);
+
         /** @var User $user */
         $user = $request->getAttribute('user', false);
 
