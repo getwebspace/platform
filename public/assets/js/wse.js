a034f13253208e5338ec38b5ea08f9e6cfa95a82
Aleksey Ilyin <alksily@outlook.com>
2023-10-03 20:46:23 +0300

    helper functions

--- public/assets/js/wse.js
+++ public/assets/js/wse.js
@@ -35,6 +35,196 @@ if (window.loader === undefined) {
     };
 }
 
+if (window.debounce === undefined) {
+    window.debounce = function (func, delay) {
+        let timerId;
+
+        return function () {
+            let context = this, args = arguments;
+
+            clearTimeout(timerId);
+
+            timerId = setTimeout(() => func.apply(context, args), delay);
+        };
+    }
+}
+
+if (window.throttle === undefined) {
+    window.throttle = function (func, delay) {
+        let lastTime = 0;
+
+        return function() {
+            let currentTime = new Date().getTime();
+
+            if (currentTime - lastTime >= delay) {
+                func.apply(this, arguments);
+                lastTime = currentTime;
+            }
+        };
+    }
+}
+
+if (window.flatten === undefined && window.unflatten === undefined) {
+    let isBuffer = (obj) => {
+        return obj &&
+            obj.constructor &&
+            (typeof obj.constructor.isBuffer === 'function') &&
+            obj.constructor.isBuffer(obj)
+    }
+
+    let keyIdentity = (key) => {
+        return key
+    }
+
+    window.flatten = function (target, opts) {
+        opts = opts || {}
+
+        let delimiter = opts.delimiter || '.'
+        let maxDepth = opts.maxDepth
+        let transformKey = opts.transformKey || keyIdentity
+        let output = {}
+
+        function step (object, prev, currentDepth) {
+            currentDepth = currentDepth || 1
+            Object.keys(object).forEach(function (key) {
+                let value = object[key]
+                let isarray = opts.safe && Array.isArray(value)
+                let type = Object.prototype.toString.call(value)
+                let isbuffer = isBuffer(value)
+                let isobject = (
+                    type === '[object Object]' ||
+                    type === '[object Array]'
+                )
+
+                let newKey = prev
+                    ? prev + delimiter + transformKey(key)
+                    : transformKey(key)
+
+                if (!isarray && !isbuffer && isobject && Object.keys(value).length &&
+                    (!opts.maxDepth || currentDepth < maxDepth)) {
+                    return step(value, newKey, currentDepth + 1)
+                }
+
+                output[newKey] = value
+            })
+        }
+
+        step(target)
+
+        return output
+    }
+
+    window.unflatten = function (target, opts) {
+        opts = opts || {}
+
+        let delimiter = opts.delimiter || '.'
+        let overwrite = opts.overwrite || false
+        let transformKey = opts.transformKey || keyIdentity
+        let result = {}
+
+        let isbuffer = isBuffer(target)
+        if (isbuffer || Object.prototype.toString.call(target) !== '[object Object]') {
+            return target
+        }
+
+        // safely ensure that the key is
+        // an integer.
+        function getkey (key) {
+            let parsedKey = Number(key)
+
+            return (
+                isNaN(parsedKey) ||
+                key.indexOf('.') !== -1 ||
+                opts.object
+            )
+                ? key
+                : parsedKey
+        }
+
+        function addKeys (keyPrefix, recipient, target) {
+            return Object.keys(target).reduce(function (result, key) {
+                result[keyPrefix + delimiter + key] = target[key]
+
+                return result
+            }, recipient)
+        }
+
+        function isEmpty (val) {
+            let type = Object.prototype.toString.call(val)
+            let isArray = type === '[object Array]'
+            let isObject = type === '[object Object]'
+
+            if (!val) {
+                return true
+            } else if (isArray) {
+                return !val.length
+            } else if (isObject) {
+                return !Object.keys(val).length
+            }
+        }
+
+        target = Object.keys(target).reduce(function (result, key) {
+            let type = Object.prototype.toString.call(target[key])
+            let isObject = (type === '[object Object]' || type === '[object Array]')
+            if (!isObject || isEmpty(target[key])) {
+                result[key] = target[key]
+                return result
+            } else {
+                return addKeys(
+                    key,
+                    result,
+                    flatten(target[key], opts)
+                )
+            }
+        }, {})
+
+        Object.keys(target).forEach(function (key) {
+            let split = key.split(delimiter).map(transformKey)
+            let key1 = getkey(split.shift())
+            let key2 = getkey(split[0])
+            let recipient = result
+
+            while (key2 !== undefined) {
+                if (key1 === '__proto__') {
+                    return
+                }
+
+                let type = Object.prototype.toString.call(recipient[key1])
+                let isobject = (
+                    type === '[object Object]' ||
+                    type === '[object Array]'
+                )
+
+                // do not write over falsey, non-undefined values if overwrite is false
+                if (!overwrite && !isobject && typeof recipient[key1] !== 'undefined') {
+                    return
+                }
+
+                if ((overwrite && !isobject) || (!overwrite && recipient[key1] == null)) {
+                    recipient[key1] = (
+                        typeof key2 === 'number' &&
+                        !opts.object
+                            ? []
+                            : {}
+                    )
+                }
+
+                recipient = recipient[key1]
+                if (split.length > 0) {
+                    key1 = getkey(split.shift())
+                    key2 = getkey(split[0])
+                }
+            }
+
+            // unflatten again for 'messy objects'
+            recipient[key1] = unflatten(target[key], opts)
+        })
+
+        return result
+    }
+}
+
+
 // ****
 // Polyfill section
 // ****
