5744de0edf02f081fba1b658608227c19c36aa5c
Aleksey Ilyin <alksily@outlook.com>
2023-10-07 00:51:10 +0300

    fix load locale files

--- src/Application/i18n.php
+++ src/Application/i18n.php
@@ -102,7 +102,9 @@ class i18n
             }
         }
 
-        static::addLocale($code, $strings);
+        if (is_array($strings)) {
+            static::addLocale($code, $strings);
+        }
     }
 
     /**
