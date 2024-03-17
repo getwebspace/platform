0bdc510a10900393795eeefaf4361aa0d6806d0e
Aleksey Ilyin <alksily@outlook.com>
2024-03-17 18:49:48 +0200

    OAuth plugin type

---
+++ src/Domain/Plugin/AbstractOAuthPlugin.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace App\Domain\Plugin;
+
+use App\Domain\AbstractPlugin;
+use App\Domain\Models\User;
+
+abstract class AbstractOAuthPlugin extends AbstractPlugin {
+    abstract public function getAuthUrl(): string;
+
+    abstract public function getToken(array $data = []): array;
+
+    abstract public function getInfo(array $data = []): array;
+
+    abstract public function callback(array $token, ?User $user = null);
+}
