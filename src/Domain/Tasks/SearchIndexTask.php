dee2d4833e94b5474c3e196252ad118f02ced3a2
Aleksey Ilyin <alksily@outlook.com>
2024-03-27 17:07:02 +0200

    fix searchindex task

--- src/Domain/Tasks/SearchIndexTask.php
+++ src/Domain/Tasks/SearchIndexTask.php
@@ -4,16 +4,16 @@ namespace App\Domain\Tasks;
 
 use App\Application\Search;
 use App\Domain\AbstractTask;
-use App\Domain\Entities\Catalog\Product;
-use App\Domain\Entities\Page;
-use App\Domain\Entities\Publication;
+use App\Domain\Models\CatalogProduct;
+use App\Domain\Models\Page;
+use App\Domain\Models\Publication;
 use Ramsey\Uuid\UuidInterface;
 
 class SearchIndexTask extends AbstractTask
 {
     public const TITLE = 'Search indexing';
 
-    public function execute(array $params = []): \App\Domain\Entities\Task
+    public function execute(array $params = []): \App\Domain\Models\Task
     {
         return parent::execute([]);
     }
@@ -25,44 +25,44 @@ class SearchIndexTask extends AbstractTask
         $pageService = $this->container->get(\App\Domain\Service\Page\PageService::class);
         foreach ($pageService->read() as $item) {
             /** @var Page $item */
-            $index[] = $this->implode('page', $item->getUuid(), Search::getIndexedText([
-                $item->getTitle(),
-                $item->getContent(),
-                $item->getMeta()['title'],
-                $item->getMeta()['description'],
-                $item->getMeta()['keywords'],
+            $index[] = $this->implode('page', $item->uuid, Search::getIndexedText([
+                $item->title,
+                $item->content,
+                $item->meta['title'] ?? '',
+                $item->meta['description'] ?? '',
+                $item->meta['keywords'] ?? '',
             ], true));
         }
 
         $publicationService = $this->container->get(\App\Domain\Service\Publication\PublicationService::class);
         foreach ($publicationService->read() as $item) {
             /** @var Publication $item */
-            $index[] = $this->implode('publication', $item->getUuid(), Search::getIndexedText([
-                $item->getTitle(),
-                $item->getContent()['short'],
-                $item->getContent()['full'],
-                $item->getMeta()['title'],
-                $item->getMeta()['description'],
-                $item->getMeta()['keywords'],
+            $index[] = $this->implode('publication', $item->uuid, Search::getIndexedText([
+                $item->title,
+                $item->content['short'] ?? '',
+                $item->content['full'] ?? '',
+                $item->meta['title'] ?? '',
+                $item->meta['description'] ?? '',
+                $item->meta['keywords'] ?? '',
             ], true));
         }
 
-        $productService = $this->container->get(\App\Domain\Service\Catalog\ProductService::class);
-        foreach ($productService->read(['status' => \App\Domain\Types\Catalog\ProductStatusType::STATUS_WORK]) as $item) {
-            /** @var Product $item */
-            $index[] = $this->implode('catalog_product', $item->getUuid(), Search::getIndexedText([
-                $item->getTitle(),
-                $item->getDescription(),
-                $item->getExtra(),
-                $item->getCountry(),
-                $item->getManufacturer(),
-                $item->getVendorCode(),
-                $item->getBarCode(),
-                $item->getMeta()['title'],
-                $item->getMeta()['description'],
-                $item->getMeta()['keywords'],
-            ], true));
-        }
+//        $productService = $this->container->get(\App\Domain\Service\Catalog\ProductService::class);
+//        foreach ($productService->read(['status' => \App\Domain\Types\Catalog\ProductStatusType::STATUS_WORK]) as $item) {
+//            /** @var CatalogProduct $item */
+//            $index[] = $this->implode('catalog_product', $item->getUuid(), Search::getIndexedText([
+//                $item->getTitle(),
+//                $item->getDescription(),
+//                $item->getExtra(),
+//                $item->getCountry(),
+//                $item->getManufacturer(),
+//                $item->getVendorCode(),
+//                $item->getBarCode(),
+//                $item->meta['title'] ?? '',
+//                $item->meta['description'] ?? '',
+//                $item->meta['keywords'] ?? '',
+//            ], true));
+//        }
 
         file_put_contents(Search::CACHE_FILE, implode(PHP_EOL, $index));
 
@@ -71,7 +71,7 @@ class SearchIndexTask extends AbstractTask
         $this->setStatusDone((string) count($index));
     }
 
-    protected function implode(string $type, UuidInterface $uuid, array $data): string
+    protected function implode(string $type, string $uuid, array $data): string
     {
         return $type . ':' . $uuid . ': ' . implode(' ', $data);
     }
