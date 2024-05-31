670a5f970142b25f2757dcdbcbea5a61138dcf89
Aleksey Ilyin <alksily@outlook.com>
2024-05-31 17:13:27 +0300

    rewrite upload files action

--- src/Application/Actions/Common/File/FileUploadAction.php
+++ src/Application/Actions/Common/File/FileUploadAction.php
@@ -6,42 +6,8 @@ class FileUploadAction extends FileAction
 {
     protected function action(): \Slim\Psr7\Response
     {
+        $models = $this->getUploadedFiles(array_key_first($_FILES));
         $path_only = $this->getParam('path_only', false);
-
-        $models = [];
-
-        if ($this->parameter('file_is_enabled', 'yes') === 'yes') {
-            foreach ($this->request->getUploadedFiles() as $field => $files) {
-                if (!is_array($files)) {
-                    $files = [$files]; // allow upload one file
-                }
-
-                $uuids = [];
-                foreach ($files as $file) {
-                    if (!$file->getError()) {
-                        $model = $this->fileService->createFromPath($file->getFilePath(), $file->getClientFilename());
-
-                        // is image
-                        if (str_starts_with($model->type, 'image/')) {
-                            $uuids[] = $model->uuid;
-                        }
-
-                        $models[$field][] = $model;
-                    }
-                }
-
-                if ($uuids) {
-                    // add task convert
-                    $task = new \App\Domain\Tasks\ConvertImageTask($this->container);
-                    $task->execute(['uuid' => $uuids]);
-
-                    // run worker
-                    \App\Domain\AbstractTask::worker($task);
-                }
-            }
-
-            $this->container->get(\App\Application\PubSub::class)->publish('common:file:upload', $models);
-        }
 
         if ($models && $path_only) {
             $file = array_shift($models)[0] ?? false;
