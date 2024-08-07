4fd60545d0681a813267824a84ec2c6040c2a931
Aleksey Ilyin <alksily@outlook.com>
2024-08-07 11:37:02 +0300

    fix reference table index

---
+++ scheme/migrations/20240807113500.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class V20240807113500 extends AbstractMigration
+{
+    public function change(): void
+    {
+        // remove old indexes and add new composite index
+        $table = $this->table('reference');
+        $table
+            ->removeIndexByName('title')
+            ->removeIndexByName('type')
+            ->addIndex(['type', 'title'], ['unique' => true])
+            ->update();
+    }
+}
